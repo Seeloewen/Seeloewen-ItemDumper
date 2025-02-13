36c9bc1f60900ef2efd164f4beb9f0fc1e51e5f4
Seeloewen <Louis.Herbert2006@gmail.com>
2025-02-13 20:33:37 +0100

    Get mod into usable state for 1.0.0-Beta1

--- src/main/java/me/seeloewen/seeloewen_itemdumper/Seeloewen_itemdumper.java
+++ src/main/java/me/seeloewen/seeloewen_itemdumper/Seeloewen_itemdumper.java
@@ -12,6 +12,7 @@ import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.config.ModConfig;
 import net.minecraftforge.fml.event.lifecycle.FMLClientSetupEvent;
+import net.minecraftforge.fml.event.lifecycle.FMLLoadCompleteEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.fml.loading.FMLPaths;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -27,7 +28,7 @@ import java.util.*;
 public class Seeloewen_itemdumper
 {
     public static final String MODID = "seeloewen_itemdumper";
-    public static final String VERSION = "1.0.0-Dev";
+    public static final String VERSION = "1.0.0-Beta1";
     public static String fileName;
     public static String modDirectory;
     private static final Logger logger = LogUtils.getLogger();
@@ -37,7 +38,66 @@ public class Seeloewen_itemdumper
     {
         IEventBus modEventBus = context.getModEventBus();
         MinecraftForge.EVENT_BUS.register(this);
-        context.registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+    }
+
+    public static void DumpItems()
+    {
+        //Create the file the game will write into
+        fileName = FMLPaths.GAMEDIR.get() + "\\seeloewen_itemdumper\\items_" + LocalDateTime.now().toString().replace(':', '-').replace('.', '-') + ".txt";
+        CreateFile(fileName);
+
+        //Get all ids and write the ids to the file
+        GetItemIds();
+
+        try
+        {
+            FileWriter writer = new FileWriter(fileName);
+
+            for (String id : itemIds)
+            {
+                writer.write(id + "\n");
+            }
+
+            writer.close();
+            logger.info("----------------------------------------------------------");
+            logger.info("[Seeloewen ItemDumper] Successfully dumped the item ids to the file.");
+            logger.info("----------------------------------------------------------");
+        }
+        catch (IOException e)
+        {
+            logger.info("[Seeloewen ItemDumper] An error occurred while dumping the items.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void CreateFile(String fileName)
+    {
+        try
+        {
+            File file = new File(fileName);
+            file.createNewFile();
+
+        }
+        catch (IOException e)
+        {
+            logger.info("An error occurred while trying to create the dump file.");
+            e.printStackTrace();
+        }
+    }
+
+    public static void GetItemIds()
+    {
+        //Go through the item registry and get the keys
+        for (Item item : ForgeRegistries.ITEMS.getValues())
+        {
+            ResourceLocation id = ForgeRegistries.ITEMS.getKey(item);
+
+            //Only add valid items (filter ids that contain a dot or slash)
+            if(id != null && !id.toString().contains(".") && !id.toString().contains("/"))
+            {
+                itemIds.add(id.toString());
+            }
+        }
     }
 
     @Mod.EventBusSubscriber(modid = MODID, bus = Mod.EventBusSubscriber.Bus.MOD, value = Dist.CLIENT)
@@ -45,9 +105,9 @@ public class Seeloewen_itemdumper
     {
 
         @SubscribeEvent
-        public static void onClientSetup(FMLClientSetupEvent event)
+        public static void onClientSetup(FMLLoadCompleteEvent event)
         {
-            logger.info(String.format("Loaded Seeloewen ItemDumper {0}", VERSION));
+            logger.info("Loaded Seeloewen ItemDumper + VERSION");
 
             //Create the directory for the mod in the .minecraft folder of the instance (if it doesn't exist already)
             File directory = new File(FMLPaths.GAMEDIR.get().toString(), "seeloewen_itemdumper");
@@ -56,64 +116,6 @@ public class Seeloewen_itemdumper
 
             //Dump the items to a file
             DumpItems();
-        }
-
-        public static void DumpItems()
-        {
-            //Create the file the game will write into
-            fileName = FMLPaths.GAMEDIR.get() + "\\seeloewen_itemdumper\\items_" + LocalDateTime.now().toString().replace(':', '-').replace('.', '-') + ".txt";
-            CreateFile(fileName);
-
-            //Get all ids and write the ids to the file
-            GetItemIds();
-
-            try
-            {
-                FileWriter writer = new FileWriter(fileName);
-
-                for (String id : itemIds)
-                {
-                    writer.write(id + "\n");
-                }
-
-                writer.close();
-                System.out.println("Successfully dumped the item ids to the file.");
-            }
-            catch (IOException e)
-            {
-                System.out.println("An error occurred while dumping the items.");
-                e.printStackTrace();
-            }
-        }
-
-        public static void CreateFile(String fileName)
-        {
-            try
-            {
-                File file = new File(fileName);
-                file.createNewFile();
-
-            }
-            catch (IOException e)
-            {
-                System.out.println("An error occurred while trying to create the dump file.");
-                e.printStackTrace();
-            }
-        }
-
-        public static void GetItemIds()
-        {
-            //Go through the item registry and get the keys
-            for (Item item : ForgeRegistries.ITEMS.getValues())
-            {
-                ResourceLocation id = ForgeRegistries.ITEMS.getKey(item);
-
-                //Only add valid items (filter ids that contain a dot or slash)
-                if(id != null && !id.toString().contains(".") && !id.toString().contains("/"))
-                {
-                    itemIds.add(id.toString());
-                }
-            }
         }
     }
 }
