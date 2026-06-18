2543320ff376bd3daa8e298c71f1aa61e2ba9c7e
Seeloewen <Louis.Herbert2006@gmail.com>
2026-06-18 05:07:21 +0200

    Initial upgrade to 26.1

--- src/main/java/me/seeloewen/seeloewen_itemdumper/Seeloewen_itemdumper.java
+++ src/main/java/me/seeloewen/seeloewen_itemdumper/Seeloewen_itemdumper.java
@@ -2,7 +2,7 @@ package me.seeloewen.seeloewen_itemdumper;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
+import net.minecraft.resources.Identifier;
 import net.neoforged.bus.api.IEventBus;
 import net.neoforged.fml.ModContainer;
 import net.neoforged.fml.common.Mod;
